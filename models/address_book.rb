42c52001ebd8fcc82be87612abe30ebe6f093a68
Joseph M O'Connor <tiluser0@gmail.com>
2017-10-02 17:29:57 +0000

    Add remove_entry method to address_book

--- models/address_book.rb
+++ models/address_book.rb
@@ -11,14 +11,33 @@ class AddressBook
     def add_entry(name, phone_number, email)
      # #9
         index = 0
-         entries.each do |entry|
+        entries.each do |entry|
          # #10
-         if name < entry.name
-             break
-         end
-         index+= 1
-     end
-     # #11
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
+            if name < entry.name
+                break
+            end
+            index+= 1
+        end
+        # #11
+        entries.insert(index, Entry.new(name, phone_number, email))
+    end
+ 
+    def remove_entry(name, phone_number, email)
+        indexes_to_delete = []
+        entries.each_with_index do |entry, index|
+            if entry.name == name && entry.phone_number == phone_number && entry.email == email
+               indexes_to_delete.push(index)
+            end
+        end
+
+        indexes_to_delete.each do |index|
+            entries.delete_at(index)
+        end
+        # puts entries.size
+    end  
+   
 end
+
+a = AddressBook.new
+a.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+a.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe AddressBook do
         it "adds only one entry to the address book" do
             book = AddressBook.new
             book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
- 
+            
             expect(book.entries.size).to eq(1)
         end
  
@@ -34,6 +34,20 @@ RSpec.describe AddressBook do
             expect(new_entry.name).to eq('Ada Lovelace')
             expect(new_entry.phone_number).to eq('010.012.1815')
             expect(new_entry.email).to eq('augusta.king@lovelace.com')
+        end
+    end
+    
+    describe "#remove_entry" do
+        it "removes the matching entry" do
+            # First add the entry, then test its removal
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            entry_size_before = book.entries.size
+            expect(entry_size_before).to eq(1)
+            
+            book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            entry_size_after = book.entries.size
+            expect(entry_size_after).to eq(0)
         end
     end
 end
